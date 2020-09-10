a89ed905cdf8d0203110e08bbee2744975f67731
marirs <251870+marirs@users.noreply.github.com>
2020-09-10 10:12:45 +0530

    Update nginx-install.sh

--- nginx/nginx-install.sh
+++ nginx/nginx-install.sh
@@ -1017,6 +1017,9 @@ case $OPTION in
         mkdir -p /etc/nginx/ssl >> /tmp/nginx-install.log 2>&1
         mkdir -p /etc/nginx/conf.d >> /tmp/nginx-install.log 2>&1
         wget -O /etc/nginx/conf.d/geo_fence.conf.default https://raw.githubusercontent.com/marirs/autoinstalls/master/nginx/conf/geo_fence.conf >> /tmp/nginx-install.log 2>&1
+	if [[ "$GEOIP2" = 'y' ]]; then
+	    wget -O /etc/nginx/conf.d/geoip2.conf https://raw.githubusercontent.com/marirs/autoinstalls/master/nginx/conf/geoip2.conf >> /tmp/nginx-install.log 2>&1
+	fi
 		# Nginx installation from source does not add an init script for systemd and logrotate
 		# Using the official systemd script and logrotate conf from nginx.org
 		if [[ ! -e /lib/systemd/system/nginx.service ]]; then
